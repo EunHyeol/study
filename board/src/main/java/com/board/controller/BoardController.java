db73b0165d66dbbc2d6a7a2422eb094e2eadeeb2
win7 <win7@win7-PC>
2020-09-15 13:50:12 +0900

    design 3rd commit
    게시판 페이징 검색 유지 기능 추가

--- board/src/main/java/com/board/controller/BoardController.java
+++ board/src/main/java/com/board/controller/BoardController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.board.service.BoardService;
 import com.board.vo.BoardVO;
@@ -54,33 +55,43 @@ public class BoardController {
 	
 	//게시판 조회
 	@RequestMapping(value = "/readView", method = RequestMethod.GET)
-	public String read(BoardVO vo, Model model) throws Exception{
+	public String read(BoardVO vo, @ModelAttribute("scri") SearchCriteria scri, Model model) throws Exception{
 		logger.info("read");
 		model.addAttribute("read", service.read(vo.getBno()));
+		model.addAttribute("scri",scri);
 		return "board/readView";
 	}
 	
 	//게시판 수정뷰
 	@RequestMapping(value = "/updateView", method=RequestMethod.GET)
-	public String updateView(BoardVO vo, Model model) throws Exception{
+	public String updateView(BoardVO vo,@ModelAttribute("scri") SearchCriteria scri, Model model) throws Exception{
 		logger.info("updateView");
 		model.addAttribute("update", service.read(vo.getBno()));
+		model.addAttribute("scri",scri);
 		return "/board/updateView";
 	}
 	
 	//게시판 수정
 	@RequestMapping(value = "/update", method=RequestMethod.POST)
-	public String update(BoardVO vo) throws Exception{
+	public String update(BoardVO vo, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr) throws Exception{
 		logger.info("update");
 		service.update(vo);
+		rttr.addAttribute("page",scri.getPage());
+		rttr.addAttribute("perPageNum",scri.getPerPageNum());
+		rttr.addAttribute("searchType",scri.getSearchType());
+		rttr.addAttribute("keyword",scri.getKeyword());
 		return "redirect:/board/list";
 	}
 	
 	//게시판 삭제
 	@RequestMapping(value = "/delete", method=RequestMethod.POST)
-	public String delete(BoardVO vo) throws Exception{
+	public String delete(BoardVO vo, @ModelAttribute("scri") SearchCriteria scri, RedirectAttributes rttr) throws Exception{
 		logger.info("delete");
 		service.delete(vo.getBno());
+		rttr.addAttribute("page",scri.getPage());
+		rttr.addAttribute("perPageNum",scri.getPerPageNum());
+		rttr.addAttribute("searchType",scri.getSearchType());
+		rttr.addAttribute("keyword",scri.getKeyword());
 		return "redirect:/board/list";
 	}
 	
